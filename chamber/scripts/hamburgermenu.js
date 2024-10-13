ce4c7600a209e47258bf25dbbf3360b76f30efcb
wjdavie <wesley.davie@gmail.com>
2024-10-12 23:14:41 -0400

    Made adjustments to the hamburger button.

--- chamber/scripts/hamburgermenu.js
+++ chamber/scripts/hamburgermenu.js
@@ -1,7 +1,11 @@
-const hamButton = document.querySelector('hamburger');
-const navigation = document.querySelector('.nav-links');
+document.addEventListener('DOMContentLoaded', () => {
+	const hamButton = document.querySelector('#menu');
+	const navigation = document.querySelector('.nav-links');
 
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+	hamButton.addEventListener('click', () => {
+		navigation.classList.toggle('open');
+		hamButton.classList.toggle('open');
+		console.log("Menu button clicked");
+		console.log(navigation.classList);
+	});
 });
