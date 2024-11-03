df237e0b6de36fda5107b7cd7baa666bb52f7318
wjdavie <wesley.davie@gmail.com>
2024-11-03 00:59:59 -0400

    Updated my Chamber page with the Chamber Discover Page

---
+++ chamber/scripts/visitdate.js
@@ -0,0 +1,23 @@
+function displayVisitMessage() {
+    const currentVisit = new Date();
+    const lastVisit = localStorage.getItem('lastVisit');
+
+    if (!lastVisit) {
+        document.getElementById('visit-message').innerText = "Welcome! Let us know if you have any questions.";
+    } else {
+        const lastVisitDate = new Date(lastVisit);
+        const timeDifference = currentVisit = lastVisitDate;
+        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        if (daysDifference < 1) {
+            document.getElementById('visit-message').innerText = "Back so soon! Awesome!";
+        } else {
+            const dayWord = daysDifference === 1 ? "day" : "days";
+            document.getElementById('visit-message').innerText = `You last visited ${daysDifference} ${dayWord} ago.`;
+        }
+    }
+
+    localStorage.setItem('lastVisit', currentVisit.toISOString());
+}
+
+displayVisitMessage();
