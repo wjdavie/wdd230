6c1e0a7e20908482e669fb69a9759988d0494f95
wjdavie <wesley.davie@gmail.com>
2024-11-15 00:54:24 -0500

    Completed the Form Build Assignment

---
+++ scripts/range.js
@@ -0,0 +1,10 @@
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("rating");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
