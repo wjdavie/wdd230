41398995e2a8ccb22f08a4e6fb31c91ce94e3ba4
wjdavie <wesley.davie@gmail.com>
2024-11-17 22:44:49 -0500

    Updated my home page with links using JS.

---
+++ scripts/links.js
@@ -0,0 +1,31 @@
+const baseURL = "https://wjdavie.github.io/wdd230/";
+const linksURL = "https://wjdavie.github.io/wdd230/data/links.json";
+const learningActivitiesCard = document.getElementById('learning-activities');
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.lessons);
+}
+
+const displayLinks = (weeks) => {
+    learningActivitiesCard.innerHTML = '';
+
+    weeks.forEach((week) => {
+        let weekLesson = document.createElement('li');
+        weekLesson.innerHTML = `<span class="number">${week.lesson}:</span>`;
+
+        week.links.forEach((link) => {
+            let linkElement = document.createElement('a');
+            linkElement.setAttribute('href', link.url);
+            linkElement.textContent = link.title;
+            linkElement.setAttribute('target', '_blank');
+
+            weekLesson.appendChild(linkElement);
+        });
+
+        learningActivitiesCard.appendChild(weekLesson);
+    });
+};
+
+getLinks();
