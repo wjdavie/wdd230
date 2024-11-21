30a975c65cf62f05ed873bb54c610eed2d525212
wjdavie <wesley.davie@gmail.com>
2024-11-20 21:34:09 -0500

    Fixed my weather icon.

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -20,13 +20,13 @@ async function apiFetch() {
     }
 }
 
+apiFetch();
+
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}@2x.png`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
     let desc = data.weather[0].description;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = `${desc.charAt(0).toUpperCase() + desc.slice(1)}`;
 }
-
-apiFetch();
