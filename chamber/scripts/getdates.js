67228a886a836ccf6e55d88f10e8880d77680794
wjdavie <wesley.davie@gmail.com>
2024-11-23 23:58:26 -0500

    Completed the join page for my chamber page

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,5 +1,10 @@
+//script to get the current year
 const currentYear = new Date().getFullYear();
 document.getElementById('currentYear').textContent = '©' + currentYear;
 
+//script to calculate the last modified date.
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = 'Last Modified ' + lastModified;
+
+//script to set the hidden input to with the current date/time
+document.getElementById('dttm').value = Date.now();
