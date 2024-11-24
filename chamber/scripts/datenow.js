bb773f0686e8ffd779f4af966f6ff2a1da90a36d
wjdavie <wesley.davie@gmail.com>
2024-11-24 01:01:36 -0500

    Created a new js file for my datenow

---
+++ chamber/scripts/datenow.js
@@ -0,0 +1,2 @@
+//script to set the hidden input to with the current date/time
+document.getElementById('dttm').value = Date.now();
--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -5,6 +5,3 @@ document.getElementById('currentYear').textContent = '©' + currentYear;
 //script to calculate the last modified date.
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = 'Last Modified ' + lastModified;
-
-//script to set the hidden input to with the current date/time
-document.getElementById('dttm').value = Date.now();
