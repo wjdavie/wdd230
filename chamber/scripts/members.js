c4ffc84582e50618739535934c4d9aabeb68c98a
wjdavie <wesley.davie@gmail.com>
2024-11-24 23:22:49 -0500

    Fixed the grid and list layouts

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -25,10 +25,10 @@ function renderCards(memberData) {
         card.innerHTML = `<img src="${member.image_file}" alt="${member.name}" class="member-image">
         <h2>${member.name}</h2>
         <p>${member.address.street} ${member.address['street 2'] ? member.address['street 2'] + ',' : ''}${member.address.city}, ${member.address.state} ${member.address.zip}</p>
-        <p>Phone: ${member.phone_number}</p>
-        <p><a href="https://${member.website_url}" target="_blank">Visit Website</a></p>
-        <p>Membership Level: ${member.membership_level}</p>
-        <p>Membership Start Date: ${member.member_start_date}</p>`;
+        <p><i>Phone:</i> ${member.phone_number}</p>
+        <p>https://${member.website_url}</p>
+        <p><i>Membership Level:</i> ${member.membership_level}</p>
+        <p><i>Membership Start Date:</i> ${member.member_start_date}</p>`;
 
         display.appendChild(card);
     });
@@ -37,14 +37,18 @@ function renderCards(memberData) {
 function renderList(memberData) {
     display.innerHTML = "";
     const ul = document.createElement("ul");
+    ul.classList.add("member-list");
+
     memberData.members.forEach(member => {
         const li = document.createElement('li');
-        li.innerHTML = `<strong>${member.name}</strong> - 
-        ${member.address.street} ${member.address['street 2'] ? member.address['street 2'] + ', ' : ''}${member.address.city}, ${member.address.state} ${member.address.zip} |
-        Phone: ${member.phone_number} |
-        <a href="https://${member.website_url}" target="_blank">Visit Website</a> |
-        Membership Level: ${member.membership_level} |
-        Membership Start Date: ${member.member_start_date}`;
+        li.classList.add("member-item");
+        li.innerHTML = `
+        <div class="member-column">${member.name}</div>
+        <div class="member-column">${member.address.street} ${member.address['street 2'] ? member.address['street 2'] + ', ' : ''}${member.address.city}, ${member.address.state} ${member.address.zip}</div>
+        <div class="member-column"><i>Phone:</i> ${member.phone_number}</div>
+        <div class="member-column">https://${member.website_url}</div>
+        <div class="member-column"><i>Membership Level:</i> ${member.membership_level}</div>
+        <div class="member-column"><i>Membership Start Date:</i> ${member.member_start_date}</div>`;
 
         ul.appendChild(li);
     });
@@ -64,6 +68,6 @@ fetchMemberData().then(data => {
     listbutton.addEventListener("click", () => {
         display.classList.add("list");
         display.classList.remove("grid");
-        renderCards(memberData);
+        renderList(memberData);
     });
 });
